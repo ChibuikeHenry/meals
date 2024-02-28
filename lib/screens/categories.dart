85d435ee84031a1ef39203a73a0792f8b027003e
ChibuikeHenry <ugwuhenry195@gmail.com>
2024-02-28 15:58:27 +0100

    meaLS APP

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -10,11 +10,10 @@ import '../models/meal.dart';
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({
     super.key,
-    required this.onToggleFavorite,required this.availableMeals,
+    required this.availableMeals,
   });
 
-  final void Function(Meal meal) onToggleFavorite;
-  final List<Meal> availableMeals;
+   final List<Meal> availableMeals;
 
   void _selectCategory(BuildContext context, Category category) {
     final filteredMeals = availableMeals
@@ -26,7 +25,6 @@ class CategoriesScreen extends StatelessWidget {
         builder: (ctx) => MealsScreen(
           title: category.title,
           meals: filteredMeals,
-          onToggleFavorite: onToggleFavorite,
         ),
       ),
     ); // Navigator.push(context, route)
--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -1,26 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:meals/models/meal.dart';
+import 'package:meals/models/providers/favourite_provider.dart';
 
-class MealDetailsScreen extends StatelessWidget {
-  const MealDetailsScreen({super.key,
+class MealDetailsScreen extends ConsumerWidget {
+  const MealDetailsScreen({
+    super.key,
     required this.meal,
-    required this.onToggleFavorite});
-
+  });
   final Meal meal;
-  final void Function (Meal meal) onToggleFavorite;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
         actions: [
           IconButton(
             onPressed: () {
-            onToggleFavorite(meal);
-          },
-      icon: Icon(Icons.star),
+              final wasAdded = ref
+                  .read(favoriteMealsProvider.notifier)
+                  .toggleFavoriteStatus(meal);
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(wasAdded ? 'Meal added as a favorite.' : 'Meals removed.'),
+                ),
+              );
+
+            },
+            icon: Icon(Icons.star),
           )
         ],
       ),
@@ -57,26 +67,27 @@ class MealDetailsScreen extends StatelessWidget {
               height: 24,
             ),
             Text(
-            'Steps',
-            style: Theme.of(context).textTheme.titleLarge!.copyWith(
-            color: Theme.of(context).colorScheme.primary,
-            fontWeight: FontWeight.bold,
-            ),
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
             ),
             SizedBox(
               height: 14,
             ),
             for (final step in meal.steps)
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 8),
-                  child: Text(
-                    step,
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground,
-                    ),
-                  ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: Text(
+                  step,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
                 ),
+              ),
           ],
         ),
       ),
